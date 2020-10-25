0f56925494a0b0ebde8e8343fc1acc81c0fa804f
irpsv <i.rpsv@live.com>
2020-10-25 23:56:57 +0500

    remove volume & echo password

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,23 @@
-FROM centos:7
+FROM centos:6
 
 WORKDIR /home/root/
 
-ENV MYSQL_PASSWORD="password"
+ENV UID=1000
+ENV GID=1000
 
 RUN yum update -y && yum install -y wget
 RUN wget http://repos.1c-bitrix.ru/yum/bitrix-env.sh && \
     chmod +x bitrix-env.sh && \
-    ./bitrix-env.sh -p -F -M '$MYSQL_PASSWORD'
+    ./bitrix-env.sh -p -H -F server1
+
+# внутри bitrix-vm работает от пользователя bitrix
+# поэтому ему нужно присвоить UID/GID внешего пользователя (по-умолчанию 1000)
+RUN usermod -u $UID bitrix && \
+    groupmod -g $GID bitrix
+
+# т.к. сменили uid то надо менять права на папки
+RUN chown -R bitrix:bitrix /tmp/php_sessions && \
+    chown -R bitrix:bitrix /tmp/php_upload
 
 COPY run.sh /home/root/
 CMD ["/bin/bash", "/home/root/run.sh"]
--- run.sh
+++ run.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
-# Note: Entry point to birix container
 
-# USE the trap if you need to also do manual cleanup after the service is stopped,
-#     or need to start multiple services in the one container
 trap "shut_down" HUP INT QUIT KILL TERM
 
 shut_down(){
-# stop service and clean up here
-
-service mysqld stop
-service nginx stop
-service httpd stop
-service sshd stop
-service crond stop
-
+    service mysqld stop
+    service nginx stop
+    service httpd stop
+    service crond stop
 }
-
-# start service in background here
-# memory=${BVAT_MEM:=262144}
 
 /etc/init.d/bvat start
 
@@ -25,14 +15,13 @@ service mysqld start
 service crond start
 service httpd start
 service nginx start
-service sshd start
 
 echo "bitrix:bitrix" | chpasswd
 
+echo "mysql root password: " && head /root/.my.cnf | grep password
 echo "[hit ctrl-c to exit] or run 'docker stop <container>'"
 while true; do sleep 1; done
 
 shut_down
 
 echo "exited $0"
-
